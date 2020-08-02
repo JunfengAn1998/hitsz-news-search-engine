8135cf830aabe105a0d1a5c054cac620739dbaed
L-M-Sherlock <l.m.kagamigawachloe@gmail.com>
2020-08-03 00:06:11 +0800

    简写

--- searchapp/data.py
+++ searchapp/data.py
@@ -10,26 +10,17 @@ class ProductData():
     just as easily load them from a database, or anywhere else.
     """
 
-    def __init__(self, id_, name, description, image, taxonomy, price):
+    def __init__(self, id_, url, title):
         self.id = id_
-        self.name = name
-        self.description = description
-        self.image = image
-        self.taxonomy = taxonomy
-        self.price = price
+        self.url = url
+        self.title = title
 
     def __str__(self):
         return textwrap.dedent("""\
             Id: {}
-            Name: {}
-            ImageUrl: {}
-            Taxonomy: {}
-            Price: ${}
-            Description:
-
-            {}
-        """).format(self.id, self.name, self.image, self.taxonomy,
-                    self.price, self.description)
+            url: {}
+            title: {}
+        """).format(self.id, self.url, self.title)
 
 
 def all_products():
@@ -45,11 +36,8 @@ def all_products():
 
         # Load the product json from the same directory as this file.
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        products_path = os.path.join(dir_path, 'products.json')
-        with open(products_path) as product_file:
-            for idx, product in enumerate(json.load(product_file)):
-                id_ = idx + 1  # ES indexes must be positive integers, so add 1
-                product_data = ProductData(id_, **product)
-                _all_products.append(product_data)
+        products_path = os.path.join(dir_path, 'news.json')
+        with open(products_path, encoding='utf-8') as product_file:
+            _all_products = [ProductData(idx + 1, **product) for idx, product in enumerate(json.load(product_file))]
 
     return _all_products
