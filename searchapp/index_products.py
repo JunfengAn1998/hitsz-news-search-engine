43f3733d3cc0b1197d8d0391f12831f06816c20d
L-M-Sherlock <l.m.kagamigawachloe@gmail.com>
2020-08-03 00:05:39 +0800

    添加 语法分析 bulk

--- searchapp/index_products.py
+++ searchapp/index_products.py
@@ -2,6 +2,7 @@ from elasticsearch import Elasticsearch
 
 from searchapp.constants import DOC_TYPE, INDEX_NAME
 from searchapp.data import all_products, ProductData
+from elasticsearch.helpers import bulk
 
 
 def main():
@@ -12,12 +13,24 @@ def main():
     es.indices.create(
         index=INDEX_NAME,
         body={
-            'mappings': {},
+            'mappings': {
+                'properties': {                             # Just a magic word.
+                    'title': {                                 # The field we want to configure.
+                        'type': 'text',                         # The kind of data we’re working with.
+                        'fields': {                             # create an analyzed field.
+                            'chinese_analyzed': {                 # Name that field `name.english_analyzed`.
+                                'type': 'text',                     # It’s also text.
+                                'analyzer': 'ik_max_word',              # And here’s the analyzer we want to use.
+                                'search_analyzer': 'ik_smart'
+                            }
+                        }
+                    }
+                }
+            },
             'settings': {},
         },
     )
-
-    index_product(es, all_products()[0])
+    bulk(es, products_to_index())
 
 
 def index_product(es, product: ProductData):
@@ -36,6 +49,20 @@ def index_product(es, product: ProductData):
     # Don't delete this! You'll need it to see if your indexing job is working,
     # or if it has stalled.
     print("Indexed {}".format("A Great Product"))
+
+
+def products_to_index():
+    for product in all_products():
+        yield {
+            '_op_type': 'create',
+            "_index": INDEX_NAME,
+            # doc_type=DOC_TYPE,
+            "_id": product.id,
+            "_source": {
+                "url": product.url,
+                "title": product.title,
+            }
+        }
 
 
 if __name__ == '__main__':
